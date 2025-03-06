5dfb0dc7254ef0459c6e3e63e42a0b02dabb2666
___techbros <94129364+Michael-Aladejobi@users.noreply.github.com>
2025-03-07 00:04:43 +0100

    Update tracker2_Script.js

--- tracker2/tracker2_Script.js
+++ tracker2/tracker2_Script.js
@@ -42,7 +42,11 @@ onValue(gps2Ref, (snapshot) => {
       <strong>HDOP:</strong> ${data.hdop} <br>
       <strong>Time:</strong> ${data.timestamp}
     `;
-
+    document.querySelector("#sensor-2 .temperature").innerHTML = `
+      <strong>Body Temperature:</strong> ${data.temperatureC}°C
+      
+    `;
+  
     // Update the iframe with the new latitude and longitude
     const iframe = document.querySelector("#mapIframe");
     iframe.src = `https://www.google.com/maps?q=${data.latitude},${data.longitude}&output=embed`;
